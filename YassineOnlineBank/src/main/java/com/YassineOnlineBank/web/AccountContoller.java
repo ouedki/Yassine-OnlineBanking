65e23892b42f22ba5d7fe645f309795cd452f4b1
yassine <yassine@192.168.1.4>
2018-03-13 08:34:19 +0000

    Added Primary and Savings account views with mapping through accountController

--- YassineOnlineBank/src/main/java/com/YassineOnlineBank/web/AccountContoller.java
+++ YassineOnlineBank/src/main/java/com/YassineOnlineBank/web/AccountContoller.java
@@ -1,20 +1,40 @@
 package com.YassineOnlineBank.web;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-//@Controller
-@RequestMapping("/account")
+import com.YassineOnlineBank.models.PrimaryAccount;
+import com.YassineOnlineBank.models.SavingsAccount;
+import com.YassineOnlineBank.models.User;
+import com.YassineOnlineBank.services.UserService;
+
+@Controller
+@RequestMapping(value="/account", method=RequestMethod.GET)
 public class AccountContoller {
 	
-	@GetMapping
-	public String getPA() {
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/primaryAccount")
+	public String getPA(Principal p, Model m) {
+		User loggedInUser = userService.findByUsername(p.getName());
+		PrimaryAccount pAcc = loggedInUser.getPrimaryAccount();
+		m.addAttribute("primaryAccount", pAcc);
+		
 		return "primaryAccount";
 	}
 	
-	@GetMapping
-	public String getSA() {
+	@GetMapping("/savingsAccount")
+	public String getSA(Principal p, Model m) {
+		User loggedInUser = userService.findByUsername(p.getName());
+		SavingsAccount sAcc = loggedInUser.getSavingsAccount();
+		m.addAttribute("savingsAccount", sAcc);
 		return "savingsAccount";
 	}
 
